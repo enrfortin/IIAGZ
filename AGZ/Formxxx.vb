fdfeed0382caeb8d9edcc5adae240e07cf04b3c0
enrfortin <enr_fortinb@hotmail.com>
2021-10-27 13:24:11 -0600

    renombramiento de recursos y cambio de proyecyo a sshagz
    cambio de recursos //Global.resource
    //My.resource

--- AGZ/Formxxx.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Formxxx
-    Private Sub Formxxx_Load(sender As Object, e As System.EventArgs) Handles MyBase.Load
-
-    End Sub
-
-    Private Sub TileNavPane1_Click(sender As Object, e As System.EventArgs) Handles TileNavPane1.Click
-
-    End Sub
-End Class
